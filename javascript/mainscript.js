78425227ea51ff8fd6be76575348f91213304dcc
R Ramana <e0398581@u.nus.edu>
2021-02-02 18:13:46 +0800

    Clean up code, add opening animations

--- javascript/mainscript.js
+++ javascript/mainscript.js
@@ -4,16 +4,15 @@ tl.staggerFrom(".topics", 3, {
     ease: Elastic.easeOut
 }, 0.3);
 
+// Intro Animation
+var slider_tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+
+slider_tl.to('.intro-text', { y: "0%", duration: 1, stagger: 0.25 });
+slider_tl.to('.slider', { y: "-100%", duration: 1.5, delay: 1 });
+slider_tl.to('.intro', { y: "-100%", duration: 1 }, "-=1.5");
+slider_tl.fromTo('header', { opacity: 0 }, { opacity: 1, duration: .25 });
+
 $(document).ready(function() {
-
-    // LOADING PAGE
-    $(window).on("load", function() {
-        $(".loader-wrapper").fadeOut("fast");
-        $("#name").addClass("animateName");
-        $("#sub").addClass("animateSub");
-        $('body').removeClass('loading');
-    });
-
     // NAVBAR TOGGLE OPEN AND CLOSE
     $('.menu').on('click', function() {
         $(this).toggleClass('open');
